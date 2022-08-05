a76eceb66e7f1cdd7abfa9e9c4cbd087d8319c26
TaylorEdelman <Taylor.edelman17@gmail.com>
2022-08-05 10:03:10 -0500

    homepage css change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import "./Home.css";
 
 export const Home = () => {
-  return <div className="homePage">Fitness Tracker</div>;
+  return <div className="homePage"><p className="test">Fitness Tracker</p></div>;
 };
 
